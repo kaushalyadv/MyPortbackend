cf4a47e5fb1c6e2e9a3b80eb4ccb3bd72b5e9112
Kaushal Yadav <kaushalyadav.lku@gmail.com>
2023-12-16 18:54:35 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const mongoURILINK = process.env.MONGO_URI_MAIN_CH; // Change to LOCAL_MONGO_URI
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURILINK);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Connection to MongoDB failed:", error.message);
+  }
+};
+
+module.exports = connectToMongo;
---
+++ index.js
@@ -0,0 +1,38 @@
+const connecttoMongo = require('./db');
+const express = require('express')
+var cors = require('cors')
+
+connecttoMongo();
+
+
+
+const app = express()
+const port = process.env.NODE_ENV || 5000
+
+app.use(cors(
+  {
+      origin: ["http://localhost:5000"],
+      methods: ["POST"],
+      credentials: true
+  }
+))
+
+app.use(express.json())
+
+// awilable routes
+
+app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/notes', require('./routes/notes'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// app.use(express.static("client/build"));
+
+
+
+app.listen(port, () => {
+  console.log(`iNotebook backend listening on port ${port}`)
+})
+
